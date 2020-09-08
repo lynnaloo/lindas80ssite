fb3c004dab01afc86c4f882b3661ba7a99d53b61
Linda Nichols <lynnaloo@gmail.com>
2020-09-08 19:33:33 -0400

    this might fix it - json

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,11 +1,11 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = "2020 is a very bad year.";
+    const responseMessage = "2020 was not a good year."
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: {
+            text: responseMessage
+        }
     };
 }
